c9db6a1be4c290f344d2c5ff7792b62de336aff7
jedondfds <jekabs.donis@gmail.com>
2020-02-10 16:36:35 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "@emotion/styled";
+import { css } from "@emotion/core";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button />
     </div>
   );
 }
+
+const ButtonBase = css`
+  padding: 0.5rem 1rem;
+  font-size: 1.25rem;
+  position: relative;
+`;
+
+const overlayStates = ({ selected = false } = {}) => css`
+  &:after {
+    content: "";
+    display: block;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    background: currentColor;
+    opacity: 0;
+  }
+
+  &:hover {
+    &:after {
+      opacity: 0.08;
+    }
+  }
+
+  &:focus {
+    &:after {
+      opacity: 0.16;
+    }
+  }
+
+  &:active {
+    &:after {
+      opacity: 0.16;
+    }
+  }
+
+  ${selected &&
+    css`
+      &:after {
+        opacity: 0.5;
+      }
+    `}
+`;
+
+const StyledButton = styled.button`
+  ${ButtonBase};
+  background: black;
+  color: hotpink;
+
+  ${overlayStates({
+    selected: p => p.happy
+  })};
+`;
+const Button = () => {
+  return <StyledButton happy>hello world</StyledButton>;
+};
 
 export default App;
